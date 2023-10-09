8282d70411f688f678760523843c0a7c36ef7969
Abhishekkarunakaran <abhishekkarunakaran100@gmail.com>
2023-10-09 21:09:37 +0530

    Day-4: completed UI

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -1,21 +1,65 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class CusButton extends StatelessWidget {
-  const CusButton({super.key});
+  String text;
+  BtnType type;
+  VoidCallback? function;
+  CusButton({
+    super.key,
+    required this.text,
+    this.type = BtnType.numbers,
+    required this.function
+  });
 
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.sizeOf(context).width;
-    
-    return Container(
-      decoration: BoxDecoration(
+
+    return InkWell(
+      onTap: function,
+      child: Container(
+        decoration: BoxDecoration(
           border: Border.all(
-        color: Color(0xfff5f5dc),
-      )),
-      width: (width - 55) / 4,
-      height: 90,
+            color: Color(0xfff5f5dc),
+          ),
+          color: getColor(type),
+        ),
+        width: (width - 55) / 4,
+        height: 85,
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+                color: Color(0xfff5f5dc),
+                fontSize: 25,
+                fontWeight: FontWeight.normal),
+          ),
+        ),
+      ),
     );
+  }
+}
+
+enum BtnType {
+  allClear,
+  operators,
+  numbers,
+  equator,
+}
+
+Color getColor(BtnType type) {
+  switch (type) {
+    case BtnType.allClear:
+      return Color(0xFFc24545);
+    case BtnType.operators:
+      return Color(0xFF292929);
+    case BtnType.equator:
+      return Color(0xFF52913f);
+    default:
+      return Colors.transparent;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'dart:developer';
+
 import 'package:cal/buttons.dart';
 import 'package:flutter/material.dart';
 
@@ -7,12 +9,34 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<MyApp> createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  String expression = "0";
+  String result = "0";
+
+  void addNum(String char) {
+    setState(() {
+      if (expression == "0") {
+      expression = char;
+    } else {
+      expression += char;
+    }
+
+    log(expression);
+    
+    });
+  }
+  
+
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'JetBrainsMono'),
@@ -33,6 +57,18 @@ class MyApp extends StatelessWidget {
                 color: Color(0xfff5f5dc),
               )),
               height: 60,
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  expression,
+                  style: TextStyle(
+                    color: Color(0xfff5f5dc),
+                    fontSize: 20,
+                  ),
+                ),
+              ),
             ),
             SizedBox(
               height: 10,
@@ -43,6 +79,18 @@ class MyApp extends StatelessWidget {
                 color: Color(0xfff5f5dc),
               )),
               height: 70,
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  result,
+                  style: TextStyle(
+                    color: Color(0xfff5f5dc),
+                    fontSize: 22,
+                  ),
+                ),
+              ),
             ),
             SizedBox(
               height: 20,
@@ -52,10 +100,25 @@ class MyApp extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    CusButton(),
-                    CusButton(),
-                    CusButton(),
-                    CusButton()
+                    CusButton(
+                      text: "AC",
+                      type: BtnType.allClear,
+                      function: () {},
+                    ),
+                    CusButton(
+                      text: "⌫",
+                      function: () {},
+                    ),
+                    CusButton(
+                      text: "^",
+                      type: BtnType.operators,
+                      function: () {},
+                    ),
+                    CusButton(
+                      text: "/",
+                      type: BtnType.operators,
+                      function: () {},
+                    ),
                   ],
                 ),
                 SizedBox(
@@ -64,10 +127,23 @@ class MyApp extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    CusButton(),
-                    CusButton(),
-                    CusButton(),
-                    CusButton()
+                    CusButton(
+                      text: "7",
+                      function: () => addNum('7'),
+                    ),
+                    CusButton(
+                      text: "8",
+                      function: () => addNum('8'),
+                    ),
+                    CusButton(
+                      text: "9",
+                      function: () => addNum('9'),
+                    ),
+                    CusButton(
+                      text: "*",
+                      type: BtnType.operators,
+                      function: () {},
+                    )
                   ],
                 ),
                 SizedBox(
@@ -76,11 +152,23 @@ class MyApp extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    
-                    CusButton(),
-                    CusButton(),
-                    CusButton(),
-                    CusButton()
+                    CusButton(
+                      text: "4",
+                      function: () => addNum('4'),
+                    ),
+                    CusButton(
+                      text: "5",
+                      function: () => addNum('5'),
+                    ),
+                    CusButton(
+                      text: "6",
+                      function: () => addNum('6'),
+                    ),
+                    CusButton(
+                      text: "-",
+                      type: BtnType.operators,
+                      function: () {},
+                    )
                   ],
                 ),
                 SizedBox(
@@ -89,11 +177,23 @@ class MyApp extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    
-                    CusButton(),
-                    CusButton(),
-                    CusButton(),
-                    CusButton()
+                    CusButton(
+                      text: "1",
+                      function: () => addNum('1'),
+                    ),
+                    CusButton(
+                      text: "2",
+                      function: () => addNum('2'),
+                    ),
+                    CusButton(
+                      text: "3",
+                      function: () => addNum('3'),
+                    ),
+                    CusButton(
+                      text: "+",
+                      type: BtnType.operators,
+                      function: () {},
+                    )
                   ],
                 ),
                 SizedBox(
@@ -102,11 +202,23 @@ class MyApp extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    
-                    CusButton(),
-                    CusButton(),
-                    CusButton(),
-                    CusButton()
+                    CusButton(
+                      text: ".",
+                      function: () => addNum('.'),
+                    ),
+                    CusButton(
+                      text: "0",
+                      function: () => addNum('0'),
+                    ),
+                    CusButton(
+                      text: "00",
+                      function: () => addNum('00'),
+                    ),
+                    CusButton(
+                      text: "=",
+                      type: BtnType.equator,
+                      function: () {},
+                    )
                   ],
                 ),
                 SizedBox(
